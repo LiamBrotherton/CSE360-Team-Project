8457c89b361e18274e61287bb19b2cc2dd3beff0
dvj910 <djain67@asu.edu>
2026-09-21 18:45:54 -0700

    Email Address thing i guess

--- src/guiAdminHome/ControllerAdminHome.java
+++ src/guiAdminHome/ControllerAdminHome.java
@@ -45,7 +45,17 @@ public class ControllerAdminHome {
 	
 	// Reference for the in-memory database so this package has access
 	private static Database theDatabase = applicationMain.FoundationsMain.database;
-
+	public static String emailAddressErrorMessage = "";	// The error message text
+	public static String emailAddressInput = "";		// The input being processed
+	public static int emailAddressIndexofError = -1;	// The index where the error was located
+	private static int state = 0;						// The current state value
+	private static int nextState = 0;					// The next state value
+	private static boolean finalState = false;			// Is this state a final state?
+	private static String inputLine = "";				// The input line
+	private static char currentChar;					// The current character in the line
+	private static int currentCharNdx;					// The index of the current character
+	private static boolean running;						// The flag that specifies if the FSM is 
+	private static int domainPartCounter = 0;			// A domain name may not exceed 63 characters
 	/**********
 	 * <p> 
 	 * 
@@ -182,13 +192,231 @@ public class ControllerAdminHome {
 	 * @param emailAddress	This String holds what is expected to be an email address
 	 */
 	protected static boolean invalidEmailAddress(String emailAddress) {
-		if (emailAddress.length() == 0) {
-			ViewAdminHome.alertEmailError.setContentText(
-					"Correct the email address and try again.");
-			ViewAdminHome.alertEmailError.showAndWait();
-			return true;
-		}
-		return false;
+		// The following are the local variable used to perform the Finite State Machine simulation
+				state = 0;							// This is the FSM state number
+				String input = emailAddress;
+				inputLine = input;					// Save the reference to the input line as a global
+				currentCharNdx = 0;					// The index of the current character
+
+				// The Finite State Machines continues until the end of the input is reached or at some 
+				// state the current character does not match any valid transition to a next state
+
+				emailAddressInput = input;			// Save a copy of the input
+
+				// Let's ensure there is input
+				if (input.length() <= 0) {
+					emailAddressErrorMessage = "There was no email address found.\n";
+					emailAddressErrorMessage = emailAddressErrorMessage + displayInput(input, 0);
+					return true;
+				}
+				currentChar = input.charAt(0);		// The current character from the above indexed position
+
+				// Let's ensure the address is not too long
+				if (input.length() > 255) {
+					emailAddressErrorMessage = "A valid email address must be no more than 255 characters.\n";
+					emailAddressErrorMessage = emailAddressErrorMessage + displayInput(input, 255);
+					return true;
+				}
+				running = true;						// Start the loop
+				System.out.println("\nCurrent Final Input  Next  DomainName\nState   State Char  State  Size");
+
+				// The Finite State Machines continues until the end of the input is reached or at some 
+				// state the current character does not match any valid transition to a next state
+				while (running) {
+					// The switch statement takes the execution to the code for the current state, where
+					// that code sees whether or not the current character is valid to transition to a
+					// next state
+					nextState = -1;						// Default to there is no next state		
+					
+					switch (state) {
+					case 0: 
+						// State 0 has just 1 valid transition.
+						// The current character is must be checked against 62 options. If any are matched
+						// the FSM must go to state 1
+						// The first and the second check for an alphabet character the third a numeric
+						if ((currentChar >= 'A' && currentChar <= 'Z')|| 		// Upper case
+								(currentChar >= 'a' && currentChar <= 'z') ||	// Lower case
+								(currentChar >= '0' && currentChar <= '9')) {	// Digit
+							nextState = 1;
+						}
+										
+						// If it is none of those characters, the FSM halts
+						else { 
+							running = false;
+						}
+						
+						break;				
+						// The execution of this state is finished
+					
+					case 1: 
+						// State 1 has three valid transitions.  
+
+						// Another local-part (LPChar) character keeps us in state 1
+						if ((currentChar >= 'A' && currentChar <= 'Z') ||
+								(currentChar >= 'a' && currentChar <= 'z') ||
+								(currentChar >= '0' && currentChar <= '9')) {
+							nextState = 1;
+						}
+						// A period sends us back to state 0 to start a new local-part segment
+						else if (currentChar == '.') {
+							nextState = 0;
+						}
+						// The @ symbol moves us into the domain part
+						else if (currentChar == '@') {
+							nextState = 2;
+						}
+						// Anything else is invalid here, so the FSM halts
+						else {
+							running = false;
+						}
+						
+						break;
+						// The execution of this state is finished
+									
+					case 2: 
+						// State 2 has one valid transition.
+						
+						// A domain-part (DPChar) character moves us into state 3
+						if ((currentChar >= 'A' && currentChar <= 'Z') ||
+								(currentChar >= 'a' && currentChar <= 'z') ||
+								(currentChar >= '0' && currentChar <= '9')) {
+							nextState = 3;
+						}
+						else {
+							running = false;
+						}
+
+						// The execution of this state is finished
+						break;
+			
+					case 3:
+						// State 3 has three valid transition.
+						
+						// Another domain-part character keeps us in state 3
+						if ((currentChar >= 'A' && currentChar <= 'Z') ||
+								(currentChar >= 'a' && currentChar <= 'z') ||
+								(currentChar >= '0' && currentChar <= '9')) {
+							nextState = 3;
+						}
+						// A period sends us back to state 2 to start a new domain segment
+						else if (currentChar == '.') {
+							nextState = 2;
+						}
+						// A hyphen moves us to state 4
+						else if (currentChar == '-') {
+							nextState = 4;
+						}
+						// Anything else halts the FSM. Note state 3 is the accepting state,
+						// so running out of input here (Eol) is handled after the loop, not
+						// treated as an error in this switch.
+						else {
+							running = false;
+						}
+
+						// The execution of this state is finished
+						break;
+
+					case 4: 
+						// State 4 has one valid transition.
+
+						// A domain-part character after the hyphen sends us back to state 3
+						if ((currentChar >= 'A' && currentChar <= 'Z') ||
+								(currentChar >= 'a' && currentChar <= 'z') ||
+								(currentChar >= '0' && currentChar <= '9')) {
+							nextState = 3;
+						}
+						else {
+							running = false;
+						}
+
+						// The execution of this state is finished
+						break;
+
+					}
+					
+					if (running) {
+						displayDebuggingInfo();
+						// When the processing of a state has finished, the FSM proceeds to the next character
+						// in the input and if there is one, it fetches that character and updates the 
+						// currentChar.  If there is no next character the currentChar is set to a blank.
+						
+						moveToNextCharacter();
+						
+						// Move to the next state
+						state = nextState;
+						nextState = -1;
+					}
+					// Should the FSM get here, the loop starts again
+
+				}
+				displayDebuggingInfo();
+				
+				System.out.println("The loop has ended.");
+
+				emailAddressIndexofError = currentCharNdx;		// Copy the index of the current character;
+				
+				// When the FSM halts, we must determine if the situation is an error or not.  That depends
+				// of the current state of the FSM and whether or not the whole string has been consumed.
+				// This switch directs the execution to separate code for each of the FSM states and that
+				// makes it possible for this code to display a very specific error message to improve the
+				// user experience.
+				switch (state) {
+				case 0:
+					// State 0 is not a final state, so we can return a very specific error message
+					emailAddressIndexofError = currentCharNdx;		// Copy the index of the current character;
+					emailAddressErrorMessage = "May only be alphanumberic.\n";
+					return true;
+
+				case 1:
+					// State 1 is not a final state, so we can return a very specific error message
+
+					emailAddressIndexofError = currentCharNdx;
+					emailAddressErrorMessage = "The character(s) after an alphanumeric character must be "
+							+ "alphanumeric, a period, or an @ symbol.\n";
+					emailAddressErrorMessage = emailAddressErrorMessage + displayInput(input, currentCharNdx);
+					return true;
+
+				case 2:
+					// State 2 is not a final state, so we can return a very specific error message
+								
+					emailAddressIndexofError = currentCharNdx;
+					emailAddressErrorMessage = "There must be at least one alphanumeric character "
+							+ "after the @ symbol.\n";
+					emailAddressErrorMessage = emailAddressErrorMessage + displayInput(input, currentCharNdx);
+					return true;
+
+				case 3:
+					// State 3 is a Final State, so this is not an error if the input is empty, otherwise
+					// we can return a very specific error message.
+
+					if (currentCharNdx<input.length()) {
+						// If not all of the string has been consumed, we point to the current character
+						// in the input line and specify what that character must be in order to move
+						// forward.
+						emailAddressIndexofError = currentCharNdx;		// Copy the index of the current character;
+						emailAddressErrorMessage = "This must be the end of the input.\n";
+						emailAddressErrorMessage = emailAddressErrorMessage + displayInput(input, currentCharNdx);
+						return true;
+					}
+					else 
+					{
+						emailAddressIndexofError = -1;
+						emailAddressErrorMessage = "";
+						return false;
+					}
+
+				case 4:
+					// State 4 is not a final state, so we can return a very specific error message. 
+
+					emailAddressIndexofError = currentCharNdx;
+					emailAddressErrorMessage = "There must be at least one alphanumeric character "
+							+ "after a hyphen in the domain name.\n";
+					emailAddressErrorMessage = emailAddressErrorMessage + displayInput(input, currentCharNdx);
+					return true;
+
+				default:
+					return false;
+				}
 	}
 	
 	/**********
@@ -213,5 +441,37 @@ public class ControllerAdminHome {
 	 */
 	protected static void performQuit() {
 		System.exit(0);
+	}
+	
+	private static String displayInput(String input, int currentCharNdx) {
+		// Display the entire input line
+		String result = input.substring(0,currentCharNdx) + "?\n";
+
+		return result;
+	}
+
+
+	private static void displayDebuggingInfo() {
+		// Display the current state of the FSM as part of an execution trace
+		if (currentCharNdx >= inputLine.length())
+			// display the line with the current state numbers aligned
+			System.out.println(((state > 99) ? " " : (state > 9) ? "  " : "   ") + state + 
+					((finalState) ? "       F   " : "           ") + "None");
+		else
+			System.out.println(((state > 99) ? " " : (state > 9) ? "  " : "   ") + state + 
+					((finalState) ? "       F   " : "           ") + "  " + currentChar + " " + 
+					((nextState > 99) ? "" : (nextState > 9) || (nextState == -1) ? "   " : "    ") + 
+					nextState + "     " + domainPartCounter);
+	}
+
+	private static void moveToNextCharacter() {
+		currentCharNdx++;
+		if (currentCharNdx < inputLine.length())
+			currentChar = inputLine.charAt(currentCharNdx);
+		else {
+			System.out.println("End of input was found!");
+			currentChar = ' ';
+			running = false;
+		}
 	}
 }
